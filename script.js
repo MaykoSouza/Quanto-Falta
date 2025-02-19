371229421f788747a2611aa4e54acf6af4c89d47
Mayko Souza <maykosouzadev@gmail.com>
2025-02-19 18:04:21 -0300

    update main

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const horas = document.getElementById("horas")
+const minutos= document.getElementById("minutos")
+const segundos = document.getElementById("segundos")
+const dia = document.getElementById("dia")
+const mes = document.getElementById("mes")
+const anos = document.getElementById("ano")
+const data = document.getElementById("data")
+const botao = document.getElementById("button")
+
+
+const dataAtual = new Date()
+hora = dataAtual.getHours()
+
+botao.addEventListener("click", ()=>{
+
+
+
+
+
+} )
+
+
+   
