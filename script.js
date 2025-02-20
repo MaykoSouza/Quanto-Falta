daaa0325498d32b08afe8f387921c1e3195124ca
Mayko Souza <maykosouzadev@gmail.com>
2025-02-19 21:24:50 -0300

    update relogio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const horas = document.getElementById("horas")
-const minutos= document.getElementById("minutos")
+const minutos = document.getElementById("minutos")
 const segundos = document.getElementById("segundos")
 const dia = document.getElementById("dia")
 const mes = document.getElementById("mes")
@@ -8,16 +8,34 @@ const data = document.getElementById("data")
 const botao = document.getElementById("button")
 
 
-const dataAtual = new Date()
-hora = dataAtual.getHours()
-
-botao.addEventListener("click", ()=>{
 
 
+function atualizaRelogio() {
+    // criar um objeto Date a cada chamada 
+
+    const agora = new Date()
+
+    //Atualiza os elementos 
+    horas.textContent = agora.getHours().toString().padStart(2, "0")
+    minutos.textContent = agora.getMinutes().toString().padStart(2, "0")
+    segundos.textContent = agora.getSeconds().toString().padStart(2, "0")
+    dia.textContent = agora.getDate().toString().padStart(2, "0")
+    mes.textContent = (agora.getMonth() + 1).toString().padStart(2, "0")
+    ano.textContent = agora.getFullYear()
+
+}
+
+// atualiza o relógio a cada segundo
+setInterval(atualizaRelogio, 1000)
 
 
+//chamando a função assim que a página é recarregada
+atualizaRelogio()
 
-} )
+
+botao.addEventListener("click", () => {
 
 
-   
+})
+
+
